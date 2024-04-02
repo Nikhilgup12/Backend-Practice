bc0a8979cbbf5853c58ccc653b129aac08c956ff
nikhi <nikhil@gmail.com>
2024-04-02 11:32:11 +0000

    error

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const dbpath = path.join(__dirname, "image.db");
+app.use(express.json());
+let db = null;
+const initialize = async () => {
+  try {
+    db = await open({
+      filename: dbpath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server is start! ");
+    });
+  } catch (e) {
+    console.log(`Error message ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initialize();
+
+app.post("/image", async (request, response) => {
+  const getDetails = request.body;
+  const { id, name, location, image_url, description } = getDetails;
+  const getImageQuery = `insert into images (id,name,location,image_url,description) 
+    values(
+        '${id}',
+         '${name}',
+         '${location}',
+         '${imageUrl}',
+         '${description}'
+    );
+    `;
+  await db.run(getImageQuery);
+  response.send("Image Successfully Added");
+});
